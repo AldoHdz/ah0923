e01ec3628cfea0b042f442bed4d9522f6d0e7a9f
Aldo Hernandez <hdzaldo@gmail.com>
2023-09-05 10:54:10 -0600

    Added toString call to PointOfSale call in main to demo basic functionality.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 
 public class Main {
-    //For the purposes of this code demo I will tag commentary on design decisions or things i'd do differently with [IfICould]
     public static void main(String[] args){
         PointOfSale pointOfSale = new PointOfSale();
         //[IfICould] - I'd push back on sending checkout dates as a "M/D/YY" formatted string. Best practices are ISO date strings or time stamp.
-        System.out.println(pointOfSale.checkout("JAKR", "9/3/15",5, 10));
+        System.out.println(pointOfSale.checkout("JAKR", "9/3/15",5, 10).toString());
     }
 }
